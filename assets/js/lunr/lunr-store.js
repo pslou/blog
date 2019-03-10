d495c17eb729facb97a73cd6920de2e127603dee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-10 23:01:09 +0000

    Deploy pslou/blog to github.com/pslou/blog.git:master

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "plan: replacing bike levers",
+        "excerpt":"I’ve been pretty excited for a while to get a vintageroadbike and I finally bought one in Seattle off Craigslist for $60 cash. I’llattach a photo sometime, but the bike is in fair condition with just a slightbit of rust. The most enjoyable yet concerning features are the downtube shiftersand...","categories": ["seattle"],
+        "tags": [],
+        "url": "paullou.me/blog/seattle/2017/05/19/bike-levers/",
+        "teaser":null},{
+        "title": "finished bike",
+        "excerpt":"So here are the photos. These are the tools and the new bike levers:And these are the old bike handles:The calipers you see below are centerpull brake calipers. My fingers were crying and my back was mad aching by the endof servicing them. I had to take multiple tries adjusting...","categories": ["seattle"],
+        "tags": [],
+        "url": "paullou.me/blog/seattle/2017/05/22/bike-work/",
+        "teaser":null},{
+        "title": "bike-tastrophe",
+        "excerpt":"So I stopped at this redlight on my “new” bike and I noticed that although Istopped, the front wheel was still rolling a little forward. Out of curiousity,I picked up my bike by the handles to understand what had just happend. Lo and behold, it turns out my front wheel...","categories": ["seattle"],
+        "tags": [],
+        "url": "paullou.me/blog/seattle/2017/05/26/biketastrophe/",
+        "teaser":null},{
+        "title": "lunch times",
+        "excerpt":"In my book, there are only three times to eat lunch:   Around 11:00 before lines form.  After 2:30 in the afternoon to avoid lines.  Or don’t eat lunch.","categories": ["seattle"],
+        "tags": [],
+        "url": "paullou.me/blog/seattle/2017/05/27/lunch/",
+        "teaser":null},{
+        "title": "appreciating oatmeal",
+        "excerpt":"Steel-cut oats, Scottish oats, old-fashioned rolled oats, quick-oats, and oat bran. These are my morning ingredients for sustained energy and discipline.Oatmeal is to me as shrimp is to Bubba from Forrest Gump.If shrimp is like the fruit of the sea, oatmeal is the meal of the oats of theland… right,...","categories": ["thoughts"],
+        "tags": [],
+        "url": "paullou.me/blog/thoughts/2019/03/10/oatmeal/",
+        "teaser":null}]
